d4436cf7ea2f337465a4f994d75017f302f269cb
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-05-26 18:13:10 -0300

    feat: persist data in json file

--- src/data/Database.js
+++ src/data/Database.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('db.json', import.meta.url)
+
 export class Database {
     #database = {}
+
+    constructor(){
+        fs.readFile(databasePath, 'utf8').then(data => {
+            this.#database = JSON.parse(data)
+        }).catch(()=>{
+            this.#persist()
+        })
+    }
+
+    #persist(){
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
 
     insert(table, data){
         if(Array.isArray(this.#database[table])){
@@ -7,6 +23,8 @@ export class Database {
         }else{
             this.#database[table] = [data]
         }
+
+        this.#persist()
         
         return data
     }
