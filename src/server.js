d75f644fc0c457f959b133f5405f858dc7a788b9
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-05-27 17:10:15 -0300

    feat: add query parameters

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "node:http"
 import { json } from "./middlewares/json.js"
 import { routes } from "./routes.js"
+import { extractQueryParams } from "./utils/extract-query-params.js"
 
 const server = http.createServer(async(req, res)=>{
     const { method, url } = req
@@ -16,7 +17,12 @@ const server = http.createServer(async(req, res)=>{
     }
     
     const routeParams = req.url.match(route.path)
-    req.params = { ...routeParams.groups }
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
     route.handler(req, res)
 })
 
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,6 +1,6 @@
 export function buildRoutePath(path){
     const routeParamsRegex = /:([a-zA-Z]+)/g
     const pathWithParams = path.replaceAll(routeParamsRegex, '(?<$1>[a-z0-9\-_]+)')
-    const pathRegex = new RegExp(`^${pathWithParams}`)
+    const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
     return pathRegex
 }
