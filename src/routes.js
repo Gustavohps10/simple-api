db042fae2664bf9ff72f21d92851cb3df4692c91
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-05-27 17:27:43 -0300

    feat: add search param

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,12 @@ export const routes = [
         method: 'GET',
         path: buildRoutePath('/users'),
         handler(req, res) {
-            console.log(req.query);
-            const users = database.select('users')
+            const { search } = req.query
+
+            const users = database.select('users', search ? {
+                name: search, 
+                email: search
+            } : null)
             return res.end(JSON.stringify(users))
         }
     },
