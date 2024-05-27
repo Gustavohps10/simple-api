96647a769bc73d11debc829a72e5ba887fe28994
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-05-27 16:48:10 -0300

    feat: add route update user

--- src/data/Database.js
+++ src/data/Database.js
@@ -33,6 +33,15 @@ export class Database {
         return this.#database[table] ?? []
     }
 
+    update(table, id, data){
+        const rowIndex = this.#database[table].findIndex(row => row.id == id)
+
+        if (rowIndex > -1) {
+            this.#database[table][rowIndex] = { id, ...data }
+            this.#persist()
+        }
+    }
+
     delete(table, id){
         const rowIndex = this.#database[table].findIndex(row => row.id == id)
 
